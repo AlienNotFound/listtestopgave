a5f76fc5111b6afc2858a85701e2cdb629333e0b
Adam Dreiøe <adr@emd.dk>
2023-10-20 11:12:30 +0200

    Code cleanup

--- ListOpgave/LinkedList.cs
+++ ListOpgave/LinkedList.cs
@@ -1,7 +1,7 @@
 namespace ListOpgave{
     public class LinkedList {
     
-        int count = -1;
+        int Count = -1;
         public Element First;
 
         public Element GetFirst() {
@@ -10,76 +10,75 @@ namespace ListOpgave{
         public void AddFirst(int data) {
             Element newElement = new();
 
-            count++;
+            Count++;
             if (First == null) {
                 newElement.Data = data;
                 newElement.Next = null;
                 First = newElement;
-                newElement.Index = count;
             } else {
                 newElement.Data = data;
                 newElement.Next = First;
                 First = newElement;
-                newElement.Index = count;
             }
         }
         public void RemoveFirst() {
-            count--;
+            Count--;
             if (First != null) {
                 First = First.Next;
             }
         }
-        public Element GetSomething(int data){
+        public Element GetElement(int data){
             bool DataNotFound = true;
             Element Pointer;
-                if (First.Data == data) {
-                    return First;
-                }else{
-                    Pointer = First;
-                    while(DataNotFound){
-                        if(Pointer.Data == data){
-                            DataNotFound = false;
-                            return Pointer;
-                        }else
-                        {
-                            if(Pointer.Next != null){
-                                Pointer = Pointer.Next;
-                            }else{
-                                return null;
-                            }                        
-                        }
+
+            if (First.Data == data) {
+                return First;
+            } else {
+                Pointer = First;
+
+                while(DataNotFound){
+                    if(Pointer.Data == data){
+                        DataNotFound = false;
+                        return Pointer;
+                    } else {
+                        if(Pointer.Next != null){
+                            Pointer = Pointer.Next;
+                        } else {
+                            return null;
+                        }                        
                     }
                 }
+            }
             return null;
         }
-        public int Count() {
-            return count;
+        public int Length() {
+            return Count;
         }
         public string Print(int data) {
-            Element something = GetSomething(data);
-            return "" + something.Data + " " + something.Next ;
+            Element element = GetElement(data);
+            return "" + element.Data + " " + element.Next ;
         }
         public void Sort() {
-            bool swapped2point0 = false;
+            bool swapped = false;
             bool isFirst = true;
             Element pointer = new();
-            while(swapped2point0 == false){
-                swapped2point0 = true;
+            while(swapped == false){
+                swapped = true;
                 if (isFirst) {
                     pointer = First;
                     isFirst = false;
                 }
                 if(pointer.Data > pointer.Next.Data){
-                    Element swapped2point0 = pointer;
+                    Element before = pointer;
                     if(pointer == First){
                         First = pointer;
                     }
                     pointer = pointer.Next;
-                    pointer.Next = swapper;
-                    swapped2point0 = false;
+                    pointer.Next = before;
+                    swapped = false;
                 }else{
                     if (pointer.Next == null) {
-                        swapped2point0 = true;
+                        swapped = true;
                     } else {
                         pointer = pointer.Next;
                     }
--- ListOpgaveTest/UnitTest1.cs
+++ ListOpgaveTest/UnitTest1.cs
@@ -2,77 +2,57 @@ using Xunit;
 using ListOpgave;
 using NuGet.Frameworks;
 
-
-public class UnitTest1
-{
-    [Fact]
-    public void AddFirstTest()
+namespace ListOpgaveTest {
+    public class UnitTest1
     {
-        // Element myElement = new Element { Data = 1 };
-        // Element secondElement = new Element { Data = 2 };
-        LinkedList MyList = new LinkedList();
+        [Fact]
+        public void AddFirstTest()
+        {
+            LinkedList MyList = new LinkedList();
+            MyList.AddFirst(1);
+            Assert.Equal(1, MyList.First.Data);
+        }
 
-        MyList.AddFirst(1);
+        [Fact]
+        public void RemoveFirstTest() {
+            LinkedList Mylist = new();
+            Mylist.AddFirst(1);
+            Mylist.RemoveFirst();
+            Assert.True(Mylist.First == null);
+        }
+        
+        [Fact]
+        public void GetElementTest(){
+            LinkedList Mylist = new();
+            Mylist.AddFirst(1);
+            Mylist.AddFirst(3);
+            Mylist.AddFirst(2);
+            Assert.True(Mylist.GetElement(3).Data == 3);
+        }
 
-        Assert.Equal(1, MyList.First.Data);
+        [Fact]
+        public void CountTest(){
+            LinkedList Mylist = new();
+            Mylist.AddFirst(1);
+            Assert.True(Mylist.Length() == 0);
+        }
 
-        // if (MyList.First == null)
-        // {
-        //     MyList.First = myElement; // Assign myElement to the First property of the LinkedList
-        // }
-        // else
-        // {
-        //     secondElement.Next = MyList.First;
-        //     MyList.First = secondElement; // Update the First property of the LinkedList
-        // }
+        [Fact]
+        public void PrintTest(){
+            LinkedList Mylist = new();
+            Mylist.AddFirst(1);
+            Assert.Equal("1 ",Mylist.Print(1));
+        }
 
-        // Assert.Equal(myElement, secondElement); // Use Assert.Equal to compare elements
-    }
-    [Fact]
-    public void RemoveFirstTest() {
-        LinkedList Mylist = new();
-
-        Mylist.AddFirst(1);
-        Mylist.RemoveFirst();
-
-        Assert.True(Mylist.First == null);
-    }
-    [Fact]
-    public void PrintTest(){
-        int printed = 0;
-        LinkedList Mylist = new();
-        Mylist.AddFirst(1);
-        printed = Mylist.First.Data;
-        Console.WriteLine(printed);
-        Assert.Equal("1 ",Mylist.Print(1));
-
-    }
-
-    [Fact]
-    public void CountTest(){
-        LinkedList Mylist = new();
-        Mylist.AddFirst(1);
-        Assert.True(Mylist.Count() == 0);
-    }
-
-    [Fact]
-    public void GetSomethingTest(){
-        LinkedList Mylist = new();
-        Mylist.AddFirst(1);
-        Mylist.AddFirst(3);
-        Mylist.AddFirst(2);
-        Assert.True(Mylist.GetSomething(3).Data == 3);
-    }
-
-    [Fact]
-    public void sortTest(){
-        LinkedList Mylist = new();
-        Mylist.AddFirst(4);
-        Mylist.AddFirst(3);
-         Mylist.AddFirst(9);
-        Mylist.AddFirst(1);       
-        Mylist.Sort();
-
-        Assert.Equal(1, Mylist.GetFirst().Data);        
+        [Fact]
+        public void SortTest(){
+            LinkedList Mylist = new();
+            Mylist.AddFirst(4);
+            Mylist.AddFirst(3);
+            Mylist.AddFirst(9);
+            Mylist.AddFirst(1);       
+            Mylist.Sort();
+            Assert.Equal(1, Mylist.GetFirst().Data);        
+        }
     }
 }
